181c4a500692fb693c13ed05f49fd826dd30e83b
p0tatoes <elojamp@gmail.com>
2024-09-25 08:16:28 +0800

    feat: separated loading messages for emergency request details and the select input for responders

--- src/app/assign/layout.jsx
+++ src/app/assign/layout.jsx
@@ -1,9 +1,3 @@
-import { Suspense } from "react";
-
 export default function AssignResponderLayout({ children }) {
-  return (
-    <div>
-      <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- src/app/assign/page.jsx
+++ src/app/assign/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import AsyncResponderMultiSelect from "../components/AsyncResponderMultiSelect";
 import RequestDetailsCard from "../components/RequestDetailsCard";
 
@@ -12,27 +13,30 @@ export default function AssignResponderPage() {
 
   async function onFormSubmit(formData) {
     // TODO: implement form logic
-    const responders = formData.get("selected_responders");
-    console.log(JSON.parse(responders));
+    const data = formData.get("selected_responders");
+    const responders = [JSON.parse(data)];
 
-    if (responders === null) {
-      // do nothing
+    if (responders === null || responders.length < 2) {
+      alert("Please select at least two (2) responders");
+      return;
     }
 
     return router.replace("/");
-
-    // console.log(formData.get("selected_responders"));
   }
 
   // TODO: fix page breaking if pressing the submit button too early after page load
   return (
     <div className="flex flex-col gap-7">
-      <RequestDetailsCard requestId={requestId} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <RequestDetailsCard requestId={requestId} />
+      </Suspense>
 
-      <form action={onFormSubmit}>
-        <AsyncResponderMultiSelect nameId="selected_responders" />
-        <button type="submit">Submit</button>
-      </form>
+      <Suspense fallback={<p>Searching for available responders...</p>}>
+        <form action={onFormSubmit}>
+          <AsyncResponderMultiSelect nameId="selected_responders" />
+          <button type="submit">Submit</button>
+        </form>
+      </Suspense>
     </div>
   );
 }
